eaf3a3853983bf5d3804a0691917bd6aab6ea921
Corey Thompson <corey.dwayne.thompson@gmail.com>
2019-09-12 06:47:44 -0500

    Reproduce notifcation location issue
    Whether a Div is full screen or fixed size, notifcations do not change location with `position:'is-top-right'` or other "top" classes. Both helper divs are attached underneath the existing Div

---
+++ my-svelte-project/src/App.svelte
@@ -0,0 +1,15 @@
+<script>
+import 'bulma/css/bulma.css'
+import { Button, Notification } from 'svelma'
+
+  function showNotification(props) {
+    Notification.create({
+      message: 'You opened this programmatically!',
+      ...props
+    })
+  }
+</script>
+<!-- <div style="width:100% !important; height:100% !important;"> -->
+<div style="width:100px !important; height:100px !important;">
+	<Button class="block" type="is-danger" on:click={() => showNotification({ type: 'is-danger', position: 'is-top-right', icon: true })}>Show Notification (danger)</Button>
+</div>
